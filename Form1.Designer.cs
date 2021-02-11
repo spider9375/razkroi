38efdf750530dc1362a3c2a91004b9145af2c1bd
Dimitar Mihaylov <32629458+spider9375@users.noreply.github.com>
2021-02-11 10:41:40 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Excel_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.templateBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.templateLbl = new System.Windows.Forms.Label();
+            this.textLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // templateBtn
+            // 
+            this.templateBtn.Location = new System.Drawing.Point(23, 12);
+            this.templateBtn.Name = "templateBtn";
+            this.templateBtn.Size = new System.Drawing.Size(75, 23);
+            this.templateBtn.TabIndex = 0;
+            this.templateBtn.Text = "Шаблон";
+            this.templateBtn.UseVisualStyleBackColor = true;
+            this.templateBtn.Click += new System.EventHandler(this.templateBtn_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Текстов файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Enabled = false;
+            this.generateBtn.Location = new System.Drawing.Point(23, 130);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(185, 23);
+            this.generateBtn.TabIndex = 2;
+            this.generateBtn.Text = "Генерирай";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // templateLbl
+            // 
+            this.templateLbl.Location = new System.Drawing.Point(23, 38);
+            this.templateLbl.Name = "templateLbl";
+            this.templateLbl.Size = new System.Drawing.Size(158, 23);
+            this.templateLbl.TabIndex = 3;
+            this.templateLbl.Click += new System.EventHandler(this.templateLbl_Click);
+            // 
+            // textLbl
+            // 
+            this.textLbl.Location = new System.Drawing.Point(23, 104);
+            this.textLbl.Name = "textLbl";
+            this.textLbl.Size = new System.Drawing.Size(158, 23);
+            this.textLbl.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 166);
+            this.Controls.Add(this.textLbl);
+            this.Controls.Add(this.templateLbl);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.templateBtn);
+            this.Name = "Form1";
+            this.Text = "Парсър";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button templateBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label templateLbl;
+        private System.Windows.Forms.Label textLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Excel_App
+{
+    public partial class Form1 : Form
+    {
+        private List<List<string>> inputData = new List<List<string>>();
+        const string SEPARATOR = "	";
+        private string template = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void templateBtn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                template = openFileDialog1.FileName;
+                templateLbl.Text = openFileDialog1.SafeFileName;
+
+                if (template.Length > 0 && inputData.Any())
+                {
+                    generateBtn.Enabled = true;
+                } else
+                {
+                    generateBtn.Enabled = false;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            inputData = new List<List<string>>();
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                textLbl.Text = openFileDialog1.SafeFileName;
+                try
+                {
+                    List<string> lines = File.ReadLines(file).ToList();
+                    lines.ForEach(line =>
+                    {
+                        inputData.Add(line.Split(SEPARATOR).ToList());
+                    });
+
+                    if (template.Length > 0 && inputData.Any())
+                    {
+                        generateBtn.Enabled = true;
+                    }
+                    else
+                    {
+                        generateBtn.Enabled = false;
+                    }
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void generateBtn_Click(object sender, EventArgs e)
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            using var package = new ExcelPackage(new FileInfo(template));
+            var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+            var collection = new List<object[]>();
+            for (int i = 0; i < inputData.Count; i++)
+            {
+                var row = inputData[i];
+                collection.Add(new object[]
+                    {
+                        "", // Nomer modul
+                        (i + 1).ToString(), // Nomer detail
+                        "", // Material
+                        row[5], // Debelina
+                        row[0], // ime detail
+                        row[6], // broi
+                        row[1], //duljina
+                        row[3], // shirina
+                        "", // flader
+                        "", // preden
+                        "", // zaden
+                        "", // lqv
+                        "", //desen
+                        "", // panti duljina
+                        "", // panti shirina
+                        "", // zabelejka
+                    });
+            }
+
+            worksheet.Cells[13, 1, collection.Count, 17].LoadFromArrays(collection);
+
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Title = "Save Excel sheet";
+            saveFileDialog1.Filter = "Excel files|*.xlsx|All files|*.*";
+            saveFileDialog1.FileName = "ExcelSheet_" + DateTime.Now.ToString("dd-MM-yyyy") + ".xlsm";
+
+            //check if user clicked the save button
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Get the FileInfo
+                FileInfo fi = new FileInfo(saveFileDialog1.FileName);
+                //write the file to the disk
+                package.SaveAs(fi);
+            }
+        }
+
+        private void templateLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
